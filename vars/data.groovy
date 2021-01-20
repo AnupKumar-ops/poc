ba51e25372289664be48ea718d48b0fee72b3859
Anup Kumar <anup.kumar2@mindtree.com>
2021-01-20 18:46:26 +0000

    add vars file

---
+++ vars/data.groovy
@@ -0,0 +1,36 @@
+#!/usr/bin/env groovy
+
+def call(String name = 'human') {
+    echo "Hello, ${name}."
+}
+
+def VendorName(){
+    echo "Cisco"
+}
+def Product(') {
+    echo "WarFiles"
+}
+def Version() {
+    echo "vnf_v1.1"
+}
+def ArtifactoryUrl() {
+    echo "http://34.71.26.245:8082/artifactory"
+}
+def ArtifactoryCredentials() {
+    echo "jfogid."
+}
+def App() {
+    echo "app2" 
+}        
+def SourceRepo() {
+    echo "https://github.com/AnupKumar-ops/jenkinsdemo"
+}       
+def SourceCredentials() {
+    echo "github"
+}
+def Registry() {
+    echo "docker.io/963287/myrepo"
+}   
+def RegistryCredential() {
+    echo "docker"
+}
